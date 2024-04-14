e99971627069b949347481a7436f598ac990a408
Girisha Sahdev <104763901+girishatechie@users.noreply.github.com>
2024-04-14 17:39:02 +0530

    Update victim_info_details.py

--- victim_info_details.py
+++ victim_info_details.py
@@ -1,6 +1,8 @@
 import gemini-1.0-pro-001.py
 import pii-detection.py
 import NotoSansKannada_Condensed-Black.ttf
+import pandas as pd
+import numpy as np
 
 victim_info_details_variables_object = {
     "vid_district_name_mask": {
