dc4162ae1376968de94b1096f5f0fbe96890a840
Mehak Sharma <122295036+CodeChivalry@users.noreply.github.com>
2024-04-14 16:52:35 +0530

    Create pii-detection.py

---
+++ pii-detection.py
@@ -0,0 +1,27 @@
+import os
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+import streamlit as st
+
+# Retrieve secrets from environment variables
+azure_key = st.secrets["AZURE_KEY"]
+azure_endpoint = st.secrets["AZURE_ENDPOINT"]
+azure_location = st.secrets["AZURE_LOCATION"]
+
+# Authenticate the client using key, endpoint, and location
+def authenticate_client():
+    ta_credential = AzureKeyCredential(azure_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=azure_endpoint, 
+            credential=ta_credential,
+            location=azure_location)  
+    return text_analytics_client
+
+# Create the client instance
+client = authenticate_client()
+
+# Detecting sensitive information (PII) from text 
+def pii_recognition(client, documents_array):
+    response = client.recognize_pii_entities(documents_array, language="kn")
+    result = [doc for doc in response if not doc.is_error]
+    return result
