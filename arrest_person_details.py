11ea0817e216038f23568c5ae1997eb579a2ed22
Mehak Sharma <122295036+CodeChivalry@users.noreply.github.com>
2024-04-14 17:38:37 +0530

    Update arrest_person_details.py

--- arrest_person_details.py
+++ arrest_person_details.py
@@ -1,6 +1,8 @@
 import NotoSansKannada_Condensed-Black.ttf
 import gemini-1.0-pro-001.py
 import pii-detection.py
+import pandas as pd
+import numpy as np
 arrest_person_details_variables_object = {
     "apd_district_name_mask": {
         "value": False,
