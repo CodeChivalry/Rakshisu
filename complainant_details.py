2d43cb1f9c907e5738269920df4f05a2bfb80f65
Girisha Sahdev <104763901+girishatechie@users.noreply.github.com>
2024-04-14 17:38:34 +0530

    Update complainant_details.py

--- complainant_details.py
+++ complainant_details.py
@@ -1,6 +1,8 @@
 import gemini-1.0-pro-001.py
 import pii-detection.py
 import NotoSansKannada_Condensed-Black.ttf
+import pandas as pd
+import numpy as np
 
 complainant_details_data_variables_object = {
     "cd_district_name_mask": {
